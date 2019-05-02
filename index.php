eb84d753234e756acfaa5758ad05b855e80da887
Marcel Ilie <milie@zoanit.com>
2019-05-02 11:42:59 +0300

    beautiful bootstrap file input

--- index.php
+++ index.php
@@ -68,6 +68,22 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+    <script>
+        $('#fitFile').on('change', function() {
+            //get the file name
+            var fileName = $(this).val();
+
+            var fakePathPresent = fileName.toLowerCase().indexOf('fakepath');
+
+            if (fakePathPresent !== -1) {
+                fileName = fileName.substring(fakePathPresent + 9);
+            }
+
+            //replace the "Choose a file" label
+            $(this).next('.custom-file-label').html(fileName);
+        })
+    </script>
 </body>
 
 </html>
